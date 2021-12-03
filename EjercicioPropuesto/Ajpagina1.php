6839e43512818f988bbd268a1ff8c17a9f91d9fa
DiegoHuamanR <72468973@ucsm.edu.pe>
2021-12-03 01:37:23 -0500

    Experiencia 2 y Ejercicio Propuesto

---
+++ EjercicioPropuesto/Ajpagina1.php
@@ -0,0 +1,27 @@
+<?PHP
+$cod= $_REQUEST['cod']; 
+if ($cod==1)
+     echo "<strong>Aries:</strong> Hoy los cambios ser�n f�sicos, personales, de car�cter, Te sentir�s impulsivo y tomar�s iniciativas. Per�odo en donde considerar�s unirte a agrupaciones de beneficencia, o de ayuda a los dem�s.";
+if ($_REQUEST['cod']==2)
+     echo "<strong>Tauro:</strong> Hoy los cambios ser�n privados, �ntimos. Recuerdos. Ayuda, solidaridad. Asuntos en lugares de retiro. Tu c�nyuge puede aportar buen status a tu vida o apoyo a tu profesi�n.";
+if ($_REQUEST['cod']==3)
+     echo "<strong>G�minis:</strong> Los asuntos de hoy tienen que ver con las amistades, reuniones, actividades con ellos. D�a esperanzado, ilusiones. Mucha energ�a sexual y fuerza emocional. Deseos dif�ciles de controlar.";
+if ($_REQUEST['cod']==4)
+     echo "<strong>Cancer:</strong> Este d�a la profesi�n y las relaciones con superiores y con tu madre ser�n de importancia. Actividad en relaci�n a estos temas. Momentos positivos con compa�eros de trabajo. Actividad laboral agradable.";
+if ($_REQUEST['cod']==5)
+     echo "<strong>Leo:</strong> Este d�a los estudios, los viajes, el extranjero y la espiritualidad ser�n lo importante. Pensamientos, religi�n y filosof�a tambi�n. Vivencias k�rmicas de la �poca te vuelven responsable tomando decisiones.";
+if ($_REQUEST['cod']==6)
+     echo "<strong>Virgo:</strong> Para este d�a toma importancia tu vida sexual, tal vez miedos, temas legales, juicios o herencias. Experiencias extra�as. Hay karma de prueba durante este per�odo en tu parte psicol�gica, gener�ndose algunos replanteos.";
+if ($_REQUEST['cod']==7)
+     echo "<strong>Libra:</strong> Hoy todo asunto tiene que ver con tu pareja, tambi�n con socios, con la gente o el p�blico. Ellos ser�n lo m�s importante del d�a. Ganancias a trav�s de especulaciones o del juego. Actividades vocacionales art�sticas.";
+if ($_REQUEST['cod']==8)
+     echo "<strong>Escorpio:</strong> Hoy todo asunto tiene que ver con temas de trabajo y de salud. Presta atenci�n a ambos. Experiencias diversas con compa�eros. Durante este per�odo tendr�s muchos recursos para ganar dinero.";
+if ($_REQUEST['cod']==9)
+     echo "<strong>Sagitario:</strong> Durante este d�a se vivir�n cambios en relaci�n a los noviazgos o a los hijos. Creatividad, actividad, diversiones y salidas. Per�odo de encuentros con personas o situaciones que te impresionan.";
+if ($_REQUEST['cod']==10)
+     echo "<strong>Capricornio:</strong> Los cambios del d�a tienen que ver con tu hogar, con la convivencia y con el padre. Asuntos relativos al car�cter en la convivencia. El karma de responsabilidad de estos momentos te acercar� al mundo de lo desconocido, mucha madurez y contacto con el m�s all�.";
+if ($_REQUEST['cod']==11)
+     echo "<strong>Acuario:</strong> Hoy todo asunto tiene que ver con el entorno inmediato, hermanos y vecinos, con la comunicaci�n, los viajes cortos o traslados frecuentes. El hablar y trasladarse ser� importante hoy. Mentalidad e ideas activas.";
+if ($_REQUEST['cod']==12)
+     echo "<strong>Piscis:</strong> Durante este d�a se vivir�n cambios en la econom�a, movimientos en los ingresos, negocios, valores. Momentos de gran fuerza y decisi�n profesionales, buscar�s el liderazgo.";
+?>
---
+++ Experiencia 2/AjaxPrueba/Default.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="AjaxPrueba._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+       
+        <asp:ScriptManager ID="ScriptManager1" runat="server">
+        </asp:ScriptManager>
+        <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+            <ContentTemplate>
+                <asp:Label ID="Label1" runat="server" Text="Label" Width="196 px"></asp:Label>
+                <br />
+                <br />
+                <asp:Timer ID="Timer1" runat="server" Interval="1000" OnTick="Page_Load">
+                </asp:Timer>
+            </ContentTemplate>
+        </asp:UpdatePanel>
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="Label" Width="196 px"></asp:Label>
+        <br />
+       
+    </div>
+</asp:Content>
